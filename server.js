8a8999b85d8930f207383c70105d97f9ebcec193
stephenkoo <stephen.koo.yr@gmail.com>
2016-06-30 21:43:51 +1000

    Create chat form. Add socket.io functions which push sockets into connections array.

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 var express = require('express'),
-	app = express(),
-	server = require('http').createServer(app);
-	io = require('socket.io').listen(server);
+	app     = express(),
+	server  = require('http').createServer(app);
+	io      = require('socket.io').listen(server);
+
+var users       = [],
+	connections = [];
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket){
+	connections.push(socket); // Push socket in connections array. 
+	console.log('Connected: %s sockets connected.', connections.length); // Number of sockets connected
+
+	// Disconnect
+	connections.splice(connections.indexOf(socket), 1);
+	console.log('Disconnnected: %s sockets connected', connections.length); // Number of remaining sockets connected
+});
+
+server.listen(process.env.PORT || 3000);
