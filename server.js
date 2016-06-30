f2124ea998ed7c8e68523186334ca05aaf512241
stephenkoo <stephen.koo.yr@gmail.com>
2016-06-30 23:08:25 +1000

    Fix issue with permanently hidden chat interface (socket.username, not sockets.username in server.js).

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ io.sockets.on('connection', function(socket){
 
 	// Disconnect
 	socket.on('disconnect', function(data){
+		// Remove username which has disconnected
+		if(!socket.username) return;
+		users.splice(users.indexOf(socket.username), 1);
+		updateUsernames();
+
 		connections.splice(connections.indexOf(socket), 1);
 		console.log('Disconnnected: %s sockets connected', connections.length); // Number of remaining sockets connected
 	});
@@ -25,8 +30,21 @@ io.sockets.on('connection', function(socket){
 		// Testing if data sent from textarea is received in this function.
 		console.log(data);
 		// Currently, this will just emit data. When we implement usernames, make this include username as well.
-		io.sockets.emit('new message', { msg: data });
+		io.sockets.emit('new message', { msg: data, user: socket.username });
 	});
+
+	// New user. Pushes username in users array.
+	socket.on('new user', function(data, callback){
+		callback(true);
+		socket.username = data;
+		users.push(socket.username);
+		updateUsernames();
+	});
+
+	// So on client side, we will need to get 'get users'
+	function updateUsernames(){
+		io.sockets.emit('get users', users);
+	};
 });
 
 server.listen(process.env.PORT || 3000);
